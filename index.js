8bee05236721f22caa418c4f9863ebdf9d4f1b83
yuiiiiet <mr.arlindapun0suy@hotmail.com>
2024-05-12 16:43:00 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-function maxArea(height) {
-  let maxArea = 0;
-  let left = 0;
-  let right = height.length - 1;
-  while (left < right) {
-    maxArea = Math.max(
-      maxArea,
-      Math.min(height[left], height[right]) * (right - left),
-    );
-    if (height[left] < height[right]) left++;
-    else right--;
-  }
-  return maxArea;
-}
+const isEven = (num) => num % 2 === 0;
